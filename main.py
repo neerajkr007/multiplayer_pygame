7f57116415b5a3774445cbf07b0805ae849d8c22
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-08-11 13:54:50 +0530

    added animation in 2 players mode
    a working animation of 2 players waking and standing

--- main.py
+++ main.py
@@ -29,35 +29,65 @@ win2 = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("testing")
 """                                            IMAGES LOAD    START                                                     """
 
-background = [pygame.image.load("bg1.jpg").convert()]
+background = [pygame.image.load("images/bg1.jpg").convert()]
 background[0] = pygame.transform.rotate(background[0], 90)
 
 """                                            IMAGES LOAD    STOP                                                      """
 Map = 0
+
+n = Network()
+player = n.getP()
 
 
 def screen_draw(win2):
     win2.blit(background[Map], [0, 0])
     player2.draw(win2)
     player.draw(win2)
+    player.move()
+    '''if player2.is_right:
+        win2.blit(standing[0], [player2.x - 20, player2.y - 10])
+    else:
+        win2.blit(standing[1], [player2.x - 20, player2.y - 10])
+    player.draw(win2)
+    if player.is_right:
+        win2.blit(standing[0], [player.x - 20, player.y - 10])
+    else:
+        win2.blit(standing[1], [player.x - 20, player.y - 10])
+'''
+    '''player.draw(win2)
+    if player.walk_count + 1 >= 24:
+        player.walk_count = 0
+        # pygame.draw.rect(win, (0, 100, 0), (player.x, player.y, player. width, player.height))
+    if player.is_left:
+        win2.blit(runL[player.walk_count // 4], [player.x - 20, player.y - 10])
+        player.walk_count += 1
+    elif player.is_right:
+        win2.blit(runR[player.walk_count // 4], [player.x - 20, player.y - 10])
+        player.walk_count += 1
+    else:
+        if player2.is_right:
+            win2.blit(standing[0], [player2.x - 20, player2.y - 10])
+        else:
+            win2.blit(standing[1], [player2.x - 20, player2.y - 10])
+        if player.is_right:
+            win2.blit(standing[0], [player.x - 20, player.y - 10])
+        else:
+            win2.blit(standing[1], [player.x - 20, player.y - 10])
+    '''
     pygame.display.update()
 
 
 clock = pygame.time.Clock()
 
 run2 = True
-n = Network()
 
 
-player = n.getP()
 while run2:
+    clock.tick(24)
     player2 = n.send(player)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run2 = False
 
-    player.move()
     screen_draw(win2)
-    clock.tick(30)
-
 pygame.quit()
