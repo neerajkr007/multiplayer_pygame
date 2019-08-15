6a195661dbfad903b955391d8e1973ca57991b55
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-08-15 20:34:03 +0530

    added launcher and fixed some melee bugs

--- exe/main.py
+++ exe/main.py
@@ -2,26 +2,55 @@ import pygame
 from network import Network
 from player import Player
 import os
-import time
 
 x = 220
 y = 100
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
 
-'''pygame.init()
-
-win = pygame.display.set_mode((400, 400))
+pygame.init()
+font = pygame.font.Font('images/Roboto-Black.ttf', 16)
+win = pygame.display.set_mode((400, 200))
 pygame.display.set_caption("Launcher")
 enter_name = font.render('Enter Player name : ', False, (255, 255, 255))
 run = True
+playername = ""
+name = ""
+
+
+def screendraw(win):
+    win.fill([30, 30, 30])
+    win.blit(enter_name, [10, 30])
+    block = font.render(playername, True, (0, 0, 0))
+    pygame.draw.rect(win, (200, 200, 200), (160, 30, 150, 20))
+    win.blit(block, [160, 30])
+    #pygame.draw.rect(win, (0, 100, 0), (150, 100, 100, 30))
+    #win.blit(font.render('START', True, (0, 0, 0)), [170, 105])
+    pygame.display.update()
+
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    win.blit(enter_name, [10, 30])
-pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.unicode.isalpha():
+                playername += event.unicode
+                # name = playername
+            elif event.key == pygame.K_BACKSPACE:
+                # name = playername
+                playername = playername[:-1]
+            elif event.key == pygame.K_RETURN:
+                name = playername
+                playername = ""
+        '''if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] >= 150 and pygame.mouse.get_pos()[1] >= 100:
+                if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 130:
+                    flag = 1
 '''
+    screendraw(win)
+pygame.quit()
+
+
 pygame.init()
 SH = 600
 SW = 950
@@ -40,6 +69,7 @@ n = Network()
 player = n.getP()
 playername = font.render('Player Name : ', False, (255, 255, 255))
 score = font.render('score : ', False, (255, 255, 255))
+player.name = name
 
 
 def screen_draw(win2):
@@ -50,6 +80,16 @@ def screen_draw(win2):
     win2.blit(score, [10, 55])
     win2.blit(playername, [SW/2 + 10, 30])
     win2.blit(score, [SW/2 + 10, 55])
+    if player.xs == 150:
+        win2.blit(name1, [200, 30])
+        win2.blit(name2, [675, 30])
+        win2.blit(score1, [580, 55])
+        win2.blit(score2, [110, 55])
+    if player.xs == 850:
+        win2.blit(name1, [675, 30])
+        win2.blit(name2, [200, 30])
+        win2.blit(score1, [110, 55])
+        win2.blit(score2, [580, 55])
     player.move(win2)
     collision(player, player2)
     if player2.melee:
@@ -75,44 +115,44 @@ def collision(player01, player02):
     if player01.melee:
         if player01.facing == 'right':
             for i in range(38):
-                if player01.x + 45 == (player02.x + i):
+                if player01.x + 45 == (player02.x + i) and ((player01.y - 10) < player02.y < player01.y + 10):
                     player02.is_dying = True
                     player02.is_dead = True
         if player01.facing == 'left':
             for i in range(48):
-                if player01.x - 15 == ((player02.x + 38) - i):
+                if player01.x - 15 == ((player02.x + 38) - i) and ((player01.y - 10) < player02.y < player01.y + 10):
                     player02.is_dying = True
                     player02.is_dead = True
-                '''else:
-                    player02.is_dying = False
-                    player02.is_dead = False'''
+
     elif player02.melee:
         if player02.facing == 'left':
             for i in range(38):
-                if player01.x + 45 == (player02.x + i):
+                if player01.x + 45 == (player02.x + i) and ((player02.y - 50) < player01.y < player02.y + 50):
                     player01.is_dying = True
                     player01.is_dead = True
         if player02.facing == 'right':
             for i in range(48):
-                if player01.x - 15 == ((player02.x + 38) - i):
+                if player01.x - 15 == ((player02.x + 38) - i) and ((player02.y - 50) < player01.y < player02.y + 50):
                     player01.is_dying = True
                     player01.is_dead = True
-                '''else:
-                    player01.is_dying = False
-                    player01.is_dead = False'''
+
     else:
         return False
 
 
 clock = pygame.time.Clock()
-
 run2 = True
 
 while run2:
     clock.tick(24)
     player2 = n.send(player)
+    name1 = font.render(player.name, False, (255, 255, 255))
+    score1 = font.render(str(player.score), False, (255, 255, 255))
+    name2 = font.render(player2.name, False, (255, 255, 255))
+    score2 = font.render(str(player2.score), False, (255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run2 = False
     screen_draw(win2)
 pygame.quit()
+
